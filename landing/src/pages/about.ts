e30cc9e3bd6c762e7e9090d6dad6578ed588a36d
Nathan Buesgens <nathan@natb1.com>
2026-05-18 20:27:45 +0000

    about update

--- landing/src/pages/about.ts
+++ landing/src/pages/about.ts
@@ -2,7 +2,7 @@ export function renderAboutHtml(): string {
   return `
     <h2>About</h2>
     <p>Nathan Buesgens is an independent contractor. One person is responsible for contracting, project management and execution. The person you give requirements to is the person who implements them.</p>
-    <p>All deliverables are coupled with the necessary documentation to host the solution using your own data (Nathan never has access to your private data), and modify the solution to meet your future requirements. Deliverables will not require business transformation or platform migration to manage. Third-party dependencies are kept to a minimum. You will recieve a solution and also a capability that stays with the business when the engagement ends.</p>
+    <p>All deliverables are coupled with the necessary documentation to modify the solution to meet your future requirements, and host the solution using your own data (Nathan never has access to your private data). Deliverables will not require business transformation or platform migration to manage. Third-party dependencies are kept to a minimum. You will recieve a solution and also a capability that stays with the business when the engagement ends.</p>
     <p>Contracts are written as a simple retainer. Work will be scoped to the size of the retainer with no long term commitments.</p>
   `;
 }
